4a54cd79c38099e7eb819a1832bb4ff9330fa053
master <kim.dhdbr@gmail.com>
2017-05-21 14:44:56 +0900

    First commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(request, response){
+  fs.readFile('../html/index.html', function(error, data){
+    response.writeHead(200, {
+      'Content-Type' : 'text/html '
+    });
+    response.end(data);
+  });
+
+}).listen(52273, function(){
+  console.log('Server is running at http://127.0.0.1:52273');
+});
